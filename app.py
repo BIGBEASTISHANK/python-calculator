3bd227fdeff8dd32f8bdf6e582abd6246f1dba9e
BIGBEASTISHANK <77890275+BIGBEASTISHANK@users.noreply.github.com>
2022-01-03 22:00:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-   if request.method == 'POST':
+    if request.method == 'POST':
         equation = str(request.form['equation'])
         result = str(eval(equation))
-        return render_template('webcalc.html', result=f'Result: {result}')
-    return render_template('index.html')
+        
+    return render_template('index.html', result=f'Result: {result}')
 
 app.run(debug=True)
