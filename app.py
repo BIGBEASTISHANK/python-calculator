c1a73ccb16850fdd9fe41e6aea8736f1eb3a5bba
BIGBEASTISHANK <77890275+BIGBEASTISHANK@users.noreply.github.com>
2022-01-03 21:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        oop = request.form['operator']
-
-        if oop == 'Add':
-            result = num1 + num2
-        elif oop == 'Multiply':
-            result = num1 * num2
-        elif oop == 'Substract':
-            result = num1 - num2
-        else:
-            result = num1 // num2
-        return render_template('index.html', result=f'Result = {result}')
-
+   if request.method == 'POST':
+        equation = str(request.form['equation'])
+        result = str(eval(equation))
+        return render_template('webcalc.html', result=f'Result: {result}')
     return render_template('index.html')
 
 app.run(debug=True)
